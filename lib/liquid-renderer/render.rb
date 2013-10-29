0aa7ac6a7291cdd4efa3c6d0cfac9cd2b58713b4
Kelley Reynolds <kelley@insidesystems.net>
2013-10-29 17:14:53 -0400

    Add ability to specify a context instead of always generating a new one

--- lib/liquid-renderer/render.rb
+++ lib/liquid-renderer/render.rb
@@ -4,14 +4,17 @@ module LiquidRenderer
     options[:assigns] ||= {}
     options[:registers] ||= {}
 
+    # Use the given context or set up a new one
+    context = options[:context] || Liquid::Context.new([options[:assigns], options[:scope]].compact, {}, options[:registers])
+
     # Render the page content
-    rendered_content = _render(content, Liquid::Context.new([options[:assigns], options[:scope]].compact, {}, options[:registers]), options[:payload])
+    rendered_content = _render(content, context, options[:payload])
 
     # If layout_content has been specified, render it and set assigns to our already-rendered content.
     # Note that we can't use 'layout' here because ActionView helpfully tries to turn any string passed to it into a file path relative to 'layouts/'
     if options[:layout_content] and options[:layout_content].kind_of?(String)
       options[:assigns][Rails.configuration.liquid_renderer['content_for_layout']] = rendered_content
-      rendered_content = _render(options[:layout_content], Liquid::Context.new([options[:assigns], options[:scope]].compact, {}, options[:registers]), options[:payload])
+      rendered_content = _render(options[:layout_content], context, options[:payload])
     end
 
     rendered_content
