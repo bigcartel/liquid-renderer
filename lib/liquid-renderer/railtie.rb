ca12fa03eda4bfeaf18a5716030372b85f34df8d
Kelley Reynolds <kelley@insidesystems.net>
2014-04-08 15:49:28 -0400

    Add status override to liquid rendering

--- lib/liquid-renderer/railtie.rb
+++ lib/liquid-renderer/railtie.rb
@@ -10,7 +10,8 @@ module LiquidRenderer
 
         ActionController::Renderers.add :liquid do |content, options|
           content_type = options.delete(:content_type) || 'text/html'
-          render :text => LiquidRenderer.render(content, options), :content_type => content_type
+          status = options.delete(:status) || :ok
+          render text: LiquidRenderer.render(content, options), content_type: content_type, status: status
         end
       end
     end
